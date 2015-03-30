4c5efcfe45c34fd5d1dc881ab7bdd89add9a181c
Carsten <carsten@cogents.io>
2015-03-30 15:36:37 -0500

    update

--- app/Modules/General/Database/Seeds/GeneralDatabaseSeeder.php
+++ app/Modules/General/Database/Seeds/GeneralDatabaseSeeder.php
@@ -13,31 +13,13 @@ class GeneralDatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
+
 		Model::unguard();
 
-/*
 		$this->call('App\Modules\General\Database\Seeds\ModuleSeeder');
-		$this->call('App\Modules\General\Database\Seeds\DepartmentsSeeder');
-		$this->call('App\Modules\General\Database\Seeds\DivisionsSeeder');
-		$this->call('App\Modules\General\Database\Seeds\EmployeeTypesSeeder');
-		$this->call('App\Modules\General\Database\Seeds\GradesSeeder');
-		$this->call('App\Modules\General\Database\Seeds\JobTitlesSeeder');
-		$this->call('App\Modules\General\Database\Seeds\PositionsSeeder');
-		$this->call('App\Modules\General\Database\Seeds\SitesSeeder');
 		$this->call('App\Modules\General\Database\Seeds\StatusesSeeder');
-		$this->call('App\Modules\General\Database\Seeds\SubjectsSeeder');
-*/
-
-
-/*
-ProfileSiteSeeder
-UserTableSeeder
-		$this->call('App\Modules\General\Database\Seeds\EmployeesSeeder');
-*/
-
-
-//		$this->call('App\Modules\General\Database\Seeds\RoomsSeeder');
 
 	}
+
 
 }
