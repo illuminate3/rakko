1f963c8587624ce516bbbb14f15aaef645a30438
Carsten <carsten@cogents.io>
2015-04-22 15:57:20 -0500

    localization

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,30 @@
 |
 */
 
+$languages = LaravelLocalization::getSupportedLocales();
+foreach($languages as $language => $values) {
+    $supportedLocales[] = $language;
+}
+
+$locale = Request::segment(1);
+if(in_array($locale, $supportedLocales)) {
+    LaravelLocalization::setLocale($locale);
+    App::setLocale($locale);
+}
+
+Route::get('/', function () {
+
+    return Redirect::to(LaravelLocalization::getCurrentLocale(), 302);
+});
+
+Route::group(array('prefix' => LaravelLocalization::getCurrentLocale(), 'before' => array('localization', 'before')), function () {
+
+    Session::put('my.locale', LaravelLocalization::getCurrentLocale());
+
+
 //Route::get('/', 'HomeController@index');
 Route::get('home', 'HomeController@index');
 
 Route::get('welcome', 'WelcomeController@index');
+
+});
--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,8 +1,39 @@
 <footer class="footer">
-	<div class="container-fluid">
-		<p class="text-muted">
-			<hr>
-			{{ Config::get('general.footer') }}
-		</p>
-	</div>
+<div class="container-fluid">
+
+	<hr>
+
+		<div class="row">
+			<div class="col-sm-4">
+				<ul class="language_bar_chooser">
+					@foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+					<li>
+						<a rel="alternate" hreflang="{!!$localeCode!!}" href="{!! LaravelLocalization::getLocalizedURL($localeCode) !!}">
+							{!! $properties['native'] !!}
+						</a>
+					</li>
+					@endforeach
+				</ul>
+			</div>
+			<div class="col-sm-4">
+				<p class="text-muted">
+					{{ Config::get('general.footer') }}
+				</p>
+			</div>
+			<div class="col-sm-4">
+{{--
+				<ul class="pull-right">
+					<li>{!! link_to_route('dashboard', 'Home') !!}</li>
+					<li>{!! link_to_route('dashboard.faq', 'Faq') !!}</li>
+					<li>{!! link_to_route('dashboard.contact', 'Contact Us') !!}</li>
+					<li><a id="gototop" class="gototop" href="#"><i class="icon-chevron-up"></i></a></li>
+					<!--#gototop-->
+				</ul>
+--}}
+			</div>
+		</div>
+
+
+
+</div>
 </footer>
